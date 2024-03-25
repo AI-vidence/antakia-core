7fe8dd2a45a6af8340812553a5947886cea00017
walidamr <walidamrouche@yahoo.fr>
2024-03-25 14:42:58 +0100

    Unit tests on class Region 100%

--- tests/data_handler/test_region.py
+++ tests/data_handler/test_region.py
@@ -34,6 +34,7 @@ class TestRegion(TestCase):
         self.r2_1 = Rule(self.v2,
                          min=1.5,
                          includes_min=False)
+        self.mask = dummy_mask(self.X)
 
     def test_init(self):
         """
@@ -43,8 +44,15 @@ class TestRegion(TestCase):
         Tests the implementation of property and setter : color, name
 
         """
-
         rule_set = RuleSet([self.r1_1])
+
+        # init a region with no mask and no ruleset
+        region = Region(self.X)
+        assert region.mask.equals(pd.Series([False] * len(self.X), index=self.X.index))
+        # init a rule with a mask
+        region = Region(self.X, mask=self.mask)
+        assert region.mask.equals(self.mask)
+        # init a region with a ruleset and no mask
         region = Region(self.X, rule_set)
         assert region.num < 0
         pd.testing.assert_series_equal(region.mask,
@@ -58,11 +66,11 @@ class TestRegion(TestCase):
         assert region.color == 'Blue'
         assert region.name == '2.00 ≤ var1 < 10.00'
 
-        #test the name of a leftouf region
+        # test the name of a leftouf region
         region2 = RegionSet(self.X)._compute_left_out_region()
         assert region2.name == 'left outs'
 
-        #test the name of a autoclustered region with and without rule set
+        # test the name of a autoclustered region with and without rule set
         region3 = Region(self.X)
         region3.auto_cluster = True
         assert region3.name == 'auto-cluster'
@@ -128,10 +136,10 @@ class TestRegion(TestCase):
     def test_get_color_series(self):
         rule_set = RuleSet([self.r1_1])
         region = Region(self.X, rule_set)
-        assert isinstance(region.get_color_serie(),pd.Series)
+        assert isinstance(region.get_color_serie(), pd.Series)
         region.color = 'grey'
-        region.get_color_serie()
-        assert region.color == 'blue'
+        assert 'blue' in list(region.get_color_serie())
+
 
 class TestModelRegion(TestCase):
 
@@ -139,12 +147,12 @@ class TestModelRegion(TestCase):
         X, y = generate_corner_dataset(10, random_seed=1234)
         X_test, y_test = generate_corner_dataset(10, random_seed=4321)
 
-        self.X = pd.DataFrame(X, columns=['var1','var2'])
+        self.X = pd.DataFrame(X, columns=['var1', 'var2'])
         self.y = pd.Series(y)
         self.X_test = pd.DataFrame(X_test)
         self.y_test = pd.Series(y_test)
         self.costumer_model = DummyModel()
-        self.mask = int_mask_to_boolean(dummy_mask(self.y,random_seed=14))
+        self.mask = int_mask_to_boolean(dummy_mask(self.y, random_seed=14))
         self.v1 = Variable(0, 'var1', 'float')
         self.r1_1 = Rule(self.v1,
                          min=2,
@@ -163,7 +171,7 @@ class TestModelRegion(TestCase):
                              self.costumer_model, score='mse')
 
     def test_to_dict(self):
-        #test with initialization with a mask
+        # test with initialization with a mask
         ModReg = ModelRegion(self.X, self.y, self.X_test, self.y_test,
                              self.costumer_model, mask=self.mask, score='MSE')
         ModReg.interpretable_models._init_models(ProblemCategory.regression)
@@ -178,7 +186,7 @@ class TestModelRegion(TestCase):
             'color': 'cyan'
         }
 
-        #test with initialization with a falsy rule and a selected model
+        # test with initialization with a falsy rule and a selected model
         # ModReg = ModelRegion(self.X, self.y, self.X_test, self.y_test,
         #                      self.costumer_model, rules=RuleSet([self.r1_1]), score='mse')
         # assert ModReg.to_dict() == {
@@ -203,6 +211,7 @@ class TestModelRegion(TestCase):
         #     'Sub-model': None,
         #     'color': 'cyan'
         # }
+
     def test_select_model(self):
         ModReg = ModelRegion(self.X, self.y, self.X_test, self.y_test,
                              self.costumer_model, mask=self.mask, score='mse')
@@ -210,11 +219,11 @@ class TestModelRegion(TestCase):
         assert ModReg.interpretable_models.selected_model == 'model_selected'
 
     def test_train_substitution_model(self):  # not ok
-        #test when X_test is None
+        # test when X_test is None
         ModReg = ModelRegion(self.X, self.y, self.X_test, self.y_test,
                              self.costumer_model, mask=self.mask, score='mse')
 
-        #test when X_test is not None
+        # test when X_test is not None
         ModReg = ModelRegion(self.X, self.y, self.X_test, self.y_test,
                              self.costumer_model, mask=self.mask, score='mse')
 
@@ -223,13 +232,14 @@ class TestModelRegion(TestCase):
                              self.costumer_model, mask=self.mask, score='mse')
         ModReg.interpretable_models._init_models(ProblemCategory.regression)
         assert ModReg.get_model('Linear Regression').name == LinearRegression().name
+
     def test_get_selected_model(self):
         ModReg = ModelRegion(self.X, self.y, self.X_test, self.y_test,
                              self.costumer_model, mask=self.mask, score='mse')
         ModReg.interpretable_models._init_models(ProblemCategory.regression)
-        #checks that the functions returns None when no model is selected
+        # checks that the functions returns None when no model is selected
         assert ModReg.get_selected_model() is None
-        #checks that the model returns the selected model
+        # checks that the model returns the selected model
         ModReg.select_model('Linear Regression')
         assert ModReg.get_selected_model().name == LinearRegression().name
 
@@ -238,20 +248,18 @@ class TestModelRegion(TestCase):
                              self.costumer_model, mask=self.mask, score='mse')
         ModReg.interpretable_models._init_models(ProblemCategory.regression)
 
-        #check that y_pred is a pd.Series of NaN when no model is selected
+        # check that y_pred is a pd.Series of NaN when no model is selected
         # y_pred = ModReg.predict(self.X)
         # assert isinstance(y_pred, pd.Series)
         # assert y_pred.isna().all()
 
         # test when a model is selected
         model_fitted = LinearRegression()
-        model_fitted.fit(X = self.X, y = self.y)
+        model_fitted.fit(X=self.X, y=self.y)
         ModReg.interpretable_models.select_model(model_fitted)
         y_pred = ModReg.predict(self.X)
         assert isinstance(y_pred, pd.Series)
         assert not y_pred.isna().all()
-
-
 
     def test_update_rule_set(self):
         ModReg = ModelRegion(self.X, self.y, self.X_test, self.y_test,
@@ -260,9 +268,9 @@ class TestModelRegion(TestCase):
         assert not ModReg.interpretable_models.models == {}
         ModReg.rules.add(self.r1_1)
         ModReg.update_rule_set(RuleSet([self.r1_2]))
-        #check that when updated, the rule set contains the new rules
+        # check that when updated, the rule set contains the new rules
         assert repr(ModReg.rules) == repr(RuleSet([self.r1_2]))
-        #checks that interpretable_models is reset
+        # checks that interpretable_models is reset
         assert ModReg.interpretable_models.models == {}
 
     def test_update_mask(self):  # not ok
@@ -277,4 +285,3 @@ class TestModelRegion(TestCase):
         assert len(ModReg.rules) == 0
         # checks that interpretable_models is reset
         assert ModReg.interpretable_models.models == {}
-
