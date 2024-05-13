e9e72ab7360436d97722300c9dd7cbc370124020
pierre <pierre@ai-vidence.com>
2024-05-13 12:01:30 +0200

    remove sample dim reduc

--- src/antakia_core/compute/dim_reduction/dim_reduc_method.py
+++ src/antakia_core/compute/dim_reduction/dim_reduc_method.py
@@ -140,12 +140,12 @@ class DimReducMethod(LongTask):
         param = self.default_parameters.copy()
         param.update(kwargs)
         dim_red_model = self.dimreduc_model(**param)
-        if hasattr(dim_red_model, 'fit'):
-            fitted_model = dim_red_model.fit(self.X.sample(n=fit_sample_num))
+        if hasattr(dim_red_model, 'fit_transform'):
+            X_red = dim_red_model.fit_transform(self.X)
+        elif hasattr(dim_red_model, 'fit'):
+            fitted_model = dim_red_model.fit(self.X)
             if hasattr(fitted_model, 'transform'):
                 X_red = fitted_model.transform(self.X)
-        elif hasattr(dim_red_model, 'fit_transform'):
-            X_red = dim_red_model.fit_transform(self.X)
         else:
             raise AttributeError(
                 "No fit method implemented for dimensionality reduction method"
